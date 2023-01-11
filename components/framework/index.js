4031d925983463f14c263bb8ecedf014b8c74680
nhan.nguyen <nhan.nguyen@uac.edu.au>
2023-01-11 20:21:53 +0700

    Log number of functions

--- components/framework/index.js
+++ components/framework/index.js
@@ -149,6 +149,8 @@ class ServerlessFramework {
       this.context.startProgress('logs');
     }
 
+    console.log('Number of functions: ', functions.length);
+
     const promises = functions.map(async (functionName) => {
       const args = ['logs', '--function', functionName];
       if (options.tail) {
